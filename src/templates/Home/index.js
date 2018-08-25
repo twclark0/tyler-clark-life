580ab82a0d58d30a23c3de12180a77004f86a652
twclark0 <tylerc644@gmail.com>
2018-08-24 23:56:43 -0600

    Profile h1 and h3 translate with mouse

--- src/templates/Home/index.js
+++ src/templates/Home/index.js
@@ -4,22 +4,26 @@ import 'regenerator-runtime/runtime'
 
 export default class Home extends React.Component {
   state = {
-    experiance: 'hidden',
-    twitter: 'hidden',
-    linkedIn: 'hidden',
-    resume: 'hidden',
-    talks: 'hidden',
-    youtube: 'hidden'
+    bubbles: {
+      experiance: 'hidden',
+      twitter: 'hidden',
+      linkedIn: 'hidden',
+      resume: 'hidden',
+      talks: 'hidden',
+      youtube: 'hidden'
+    },
+    mouse: { x: 0, y: 0 },
+    mouseMove: false
   }
 
   composeClasses(name) {
     return `${css.bubble} ${
-      this.state[name] === 'hidden' ? css.hide : css[`bubble--${name}`]
+      this.state.bubbles[name] === 'hidden' ? css.hide : css[`bubble--${name}`]
     }`
   }
 
   composeFontSize(name) {
-    return `${this.state[name] ? css['bubble--p'] : ''}`
+    return `${this.state.bubbles[name] ? css['bubble--p'] : ''}`
   }
 
   componentDidMount() {
@@ -30,7 +34,7 @@ export default class Home extends React.Component {
 
     const self = this
     function* myGen() {
-      yield* [...Object.keys(self.state)]
+      yield* [...Object.keys(self.state.bubbles)]
       yield false
     }
 
@@ -40,7 +44,10 @@ export default class Home extends React.Component {
       const { value } = gen.next()
       if (value) {
         this.setState({
-          [value]: 'show'
+          bubbles: {
+            ...this.state.bubbles,
+            [value]: 'show'
+          }
         })
       } else {
         clearInterval(showInterval)
@@ -48,29 +55,59 @@ export default class Home extends React.Component {
     }
     setInterval(showInterval, 200)
   }
+
+  translateMouse = e => {
+    const {
+      left,
+      top,
+      width,
+      height
+    } = this.containerRef.getBoundingClientRect()
+    const x = (e.clientX - (left + width / 2)) * 0.007
+    const y = (e.clientY - (top + height / 2)) * 0.007
+    this.setState({ mouse: { x, y }, mouseMove: true })
+  }
+
   render() {
+    const translation = {
+      transform: `translate(${this.state.mouse.x}px, ${this.state.mouse.y}px)`
+    }
     return (
-      <section className={css.container}>
-        <div className={css.contentContainer}>
+      <section className={css.container} onMouseMove={this.translateMouse}>
+        <div
+          className={css.contentContainer}
+          ref={el => (this.containerRef = el)}>
           <div className={css.profile}>
-            <h1>Tyler Clark</h1>
-            <h3>Fullstack Developer</h3>
+            <h1 style={{ ...translation }}>Tyler Clark</h1>
+            <h3 style={{ ...translation }}>Fullstack Developer</h3>
           </div>
           <div className={css.bubblesContainer}>
             <div className={this.composeClasses('experiance')}>
-              <p style={{fontSize: '0px'}} className={this.composeFontSize('experiance')}>experiance</p>
+              <p
+                style={{ fontSize: '0px' }}
+                className={this.composeFontSize('experiance')}>
+                Experiance
+              </p>
             </div>
             <div className={this.composeClasses('youtube')}>
-              <i style={{ fontSize: '0px' }} className='fab fa-youtube' />
+              <i style={{ fontSize: '0px' }} className="fab fa-youtube" />
             </div>
             <div className={this.composeClasses('talks')}>
-              <p style={{fontSize: '0px'}} className={this.composeFontSize('talks')}>talks</p>
+              <p
+                style={{ fontSize: '0px' }}
+                className={this.composeFontSize('talks')}>
+                Talks
+              </p>
             </div>
             <div className={this.composeClasses('linkedIn')}>
               <i style={{ fontSize: '0px' }} className="fab fa-linkedin-in" />
             </div>
             <div className={this.composeClasses('resume')}>
-              <p style={{fontSize: '0px'}} className={this.composeFontSize('resume')}>resume</p>
+              <p
+                style={{ fontSize: '0px' }}
+                className={this.composeFontSize('resume')}>
+                Resume
+              </p>
             </div>
             <div className={this.composeClasses('twitter')}>
               <i style={{ fontSize: '0px' }} className="fab fa-twitter" />
