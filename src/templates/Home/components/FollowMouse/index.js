47786ad947044d23aa48be1e066d15071dad5692
twclark0 <tylerc644@gmail.com>
2018-08-28 23:27:36 -0600

    Everything follows mouse at different sensitivity

--- src/templates/Home/components/FollowMouse/index.js
+++ src/templates/Home/components/FollowMouse/index.js
@@ -5,13 +5,20 @@ export default class FollowMouse extends React.Component {
   translateMouse = ({ x, y }) => {
     if (this.dom) {
       const { left, top, width, height } = this.dom.getBoundingClientRect()
-      const mouseX = (x - (left + width / 2)) * 0.009
-      const mouseY = (y - (top + height / 2)) * 0.009
+      const mouseX =
+        this.props.transX + (x - (left + width / 2)) * this.props.sensitivity
+      const mouseY =
+        this.props.transY + (y - (top + height / 2)) * this.props.sensitivity
       return { transform: `translate(${mouseX}px, ${mouseY}px)` }
     }
-    return { transform: `translate(0px, 0px)` }
+    return {
+      transform: `translate(${this.props.defaults.x}px, ${
+        this.props.defaults.y
+      }px)`
+    }
   }
   render() {
+    const otherInlineStyles = this.props.inline ? { ...this.props.inline } : {}
     return (
       <MouseContext.Consumer>
         {context =>
@@ -19,10 +26,16 @@ export default class FollowMouse extends React.Component {
             ref: dom => {
               this.dom = dom
             },
-            style: this.translateMouse(context)
+            style: { ...this.translateMouse(context), ...otherInlineStyles }
           })
         }
       </MouseContext.Consumer>
     )
   }
+}
+FollowMouse.defaultProps = {
+  transX: 0,
+  transY: 0,
+  sensitivity: 0.009,
+  defaults: { x: 0, y: 0 }
 }
--- src/templates/Home/index.js
+++ src/templates/Home/index.js
@@ -14,7 +14,8 @@ export default class Home extends React.Component {
       resume: 'hidden',
       talks: 'hidden',
       youtube: 'hidden'
-    }
+    },
+    timer: true
   }
 
   composeClasses(name) {
@@ -25,6 +26,14 @@ export default class Home extends React.Component {
 
   composeFontSize(name) {
     return `${this.state.bubbles[name] ? css['bubble--p'] : ''}`
+  }
+
+  composeTransition() {
+    return {
+      transition: `width 0.6s, height 0.6s, transform ${
+        this.state.timer ? '1s' : ' 0.1s'
+      }`
+    }
   }
 
   componentDidMount() {
@@ -54,6 +63,11 @@ export default class Home extends React.Component {
         clearInterval(showInterval)
       }
     }
+    setTimeout(() => {
+      this.setState({
+        timer: false
+      })
+    }, 2000)
     setInterval(showInterval, 200)
   }
 
@@ -61,51 +75,87 @@ export default class Home extends React.Component {
     return (
       <Section>
         <div className={css.imgContainer} />
-        <FollowMouse>
+        <FollowMouse sensitivity={0.009} defaults={{ x: -2.62, y: -4.91 }}>
           <img className={css.profileImg} src={tyler} />
         </FollowMouse>
         <div
           className={css.contentContainer}
           ref={el => (this.containerRef = el)}>
           <div className={css.profile}>
-            <FollowMouse>
+            <FollowMouse sensitivity={0.009} defaults={{ x: -9.41, y: -3.38 }}>
               <h1>Tyler Clark</h1>
             </FollowMouse>
-            <FollowMouse>
+            <FollowMouse sensitivity={0.007} defaults={{ x: -7.34, y: -2.9 }}>
               <h3>Fullstack Developer</h3>
             </FollowMouse>
           </div>
           <div className={css.bubblesContainer}>
-            <div className={this.composeClasses('experiance')}>
-              <p
-                style={{ fontSize: '0px' }}
-                className={this.composeFontSize('experiance')}>
-                Experiance
-              </p>
-            </div>
-            <div className={this.composeClasses('youtube')}>
-              <i style={{ fontSize: '0px' }} className="fab fa-youtube" />
-            </div>
-            <div className={this.composeClasses('talks')}>
-              <p
-                style={{ fontSize: '0px' }}
-                className={this.composeFontSize('talks')}>
-                Talks
-              </p>
-            </div>
-            <div className={this.composeClasses('linkedIn')}>
-              <i style={{ fontSize: '0px' }} className="fab fa-linkedin-in" />
-            </div>
-            <div className={this.composeClasses('resume')}>
-              <p
-                style={{ fontSize: '0px' }}
-                className={this.composeFontSize('resume')}>
-                Resume
-              </p>
-            </div>
-            <div className={this.composeClasses('twitter')}>
-              <i style={{ fontSize: '0px' }} className="fab fa-twitter" />
-            </div>
+            <FollowMouse
+              transX={0}
+              transY={this.state.bubbles.experiance === 'hidden' ? 0 : -10}
+              inline={this.composeTransition()}
+              sensitivity={0.009}>
+              <div className={this.composeClasses('experiance')}>
+                <p
+                  style={{ fontSize: '0px' }}
+                  className={this.composeFontSize('experiance')}>
+                  Experiance
+                </p>
+              </div>
+            </FollowMouse>
+            <FollowMouse
+              transX={this.state.bubbles.youtube === 'hidden' ? 0 : -85}
+              transY={this.state.bubbles.youtube === 'hidden' ? 0 : 60}
+              inline={this.composeTransition()}
+              sensitivity={0.008}>
+              <div className={this.composeClasses('youtube')}>
+                <i style={{ fontSize: '0px' }} className="fab fa-youtube" />
+              </div>
+            </FollowMouse>
+            <FollowMouse
+              transX={this.state.bubbles.talks === 'hidden' ? 0 : -120}
+              transY={this.state.bubbles.talks === 'hidden' ? 0 : -35}
+              inline={this.composeTransition()}
+              sensitivity={0.006}>
+              <div className={this.composeClasses('talks')}>
+                <p
+                  style={{ fontSize: '0px' }}
+                  className={this.composeFontSize('talks')}>
+                  Talks
+                </p>
+              </div>
+            </FollowMouse>
+            <FollowMouse
+              transX={this.state.bubbles.linkedIn === 'hidden' ? 0 : -50}
+              transY={this.state.bubbles.linkedIn === 'hidden' ? 0 : 120}
+              inline={this.composeTransition()}
+              sensitivity={0.01}>
+              <div className={this.composeClasses('linkedIn')}>
+                <i style={{ fontSize: '0px' }} className="fab fa-linkedin-in" />
+              </div>
+            </FollowMouse>
+            <FollowMouse
+              transX={this.state.bubbles.resume === 'hidden' ? 0 : 70}
+              transY={this.state.bubbles.resume === 'hidden' ? 0 : 110}
+              inline={this.composeTransition()}
+              sensitivity={0.009}>
+              <div className={this.composeClasses('resume')}>
+                <p
+                  style={{ fontSize: '0px' }}
+                  className={this.composeFontSize('resume')}>
+                  Resume
+                </p>
+              </div>
+            </FollowMouse>
+            <FollowMouse
+              transX={this.state.bubbles.twitter === 'hidden' ? 0 : 100}
+              transY={this.state.bubbles.twitter === 'hidden' ? 0 : 0}
+              inline={this.composeTransition()}
+              sensitivity={0.011}>
+              <div className={this.composeClasses('twitter')}>
+                <i style={{ fontSize: '0px' }} className="fab fa-twitter" />
+              </div>
+            </FollowMouse>
           </div>
         </div>
       </Section>
